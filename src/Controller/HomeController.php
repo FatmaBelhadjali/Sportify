69ddc6bec3c6178fdc420acdc72d8eaedb21c49b
YassineMz1 <ahmedyassine.mzoughi@esprit.tn>
2024-04-16 11:17:48 +0200

    2nd commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UtilisateursController extends AbstractController
+class HomeController extends AbstractController
 {
 
 #[Route('/', name: 'app_utilisateurs_index', methods: ['GET'])]
